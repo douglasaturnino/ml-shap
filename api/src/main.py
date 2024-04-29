04cb3d4802bbe0fdce6194737071a8aceeb80dcb
Douglas Saturnino <douglasaturnino@gmail.com>
2024-04-29 10:47:21 -0300

    Adicionado a docstring para o arquivo main.py e models.py

--- api/src/main.py
+++ api/src/main.py
@@ -6,12 +6,22 @@ from config.config import config
 from fastapi import FastAPI, Request
 from src.models import Data
 from src.process import DataProcess
+from typing import Any, Dict, List
 
 app = FastAPI()
 
 
 @app.post(config["api"]["path_predict"])
-def predict(data: Data):
+def predict(data: Data)-> Dict:
+    """
+    Endpoint para previsão de dados enviados via POST.
+
+    Args:
+        data (Data): Dados de entrada.
+
+    Returns:
+        dict: Resultados das previsões.
+    """
     try:
         data = pd.DataFrame.from_dict([data.model_dump()])
         return DataProcess().process_data(data)
@@ -20,7 +30,16 @@ def predict(data: Data):
 
 
 @app.post(config["api"]["path_predict_file"])
-async def predict_file(request: Request):
+async def predict_file(request: Request) -> List[Any]:
+    """
+    Endpoint para previsão de dados enviados via POST como arquivo.
+
+    Args:
+        request (Request): Requisição HTTP.
+
+    Returns:
+        dict: Resultados das previsões.
+    """
     try:
         data = await request.json()
         data = json.loads(data)
--- api/src/models.py
+++ api/src/models.py
@@ -1,7 +1,24 @@
 from typing import List, Union
 
 from pydantic import BaseModel
+"""
+Definições de modelos Pydantic para representar a saída de um modelo SHAP e os dados de entrada.
 
+Classes:
+    - ShapOutput: Uma classe Pydantic para representar a saída de um modelo SHAP, incluindo os valores SHAP,
+      valores base e os dados de entrada.
+    - Data: Uma classe Pydantic para representar os dados de entrada do modelo.
+
+Exemplo de uso:
+    # Criando uma instância do modelo ShapOutput
+    shap_output = ShapOutput(values=[[0.1, 0.2], [0.3, 0.4]],
+                             base_values=[0.349, 0.349],
+                             data=[[0.099, 0.275, 0.472, 30, 1000]])
+
+    # Criando uma instância do modelo Data
+    data_input = Data(dependentes='S', estado_civil='casado', idade=30,
+                      cheque_sem_fundo='N', valor_emprestimo=1000.0)
+"""
 
 class ShapOutput(BaseModel):
     values: List[List[float]]
